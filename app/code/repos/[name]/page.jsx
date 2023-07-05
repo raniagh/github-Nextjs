58bc39a4aeccd5d4aeddc87c49745e8e9dfdfac4
Your Name <rghzaiel@ggn-group.com>
2023-07-05 11:06:12 +0100

    repo & repo dirs

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,8 +1,15 @@
+import Repo from "@/app/components/Repo";
+import RepoDirs from "@/app/components/RepoDirs";
+import Link from "next/link";
+
 const RepoPage = ({ params: { name } }) => {
   return (
     <div className="card">
-      <h2>{name}</h2>
-      <p>Repo Details</p>
+      <Link href="/code/repos" className="btn btn-back">
+        Back To Repositories
+      </Link>
+      <Repo name={name} />
+      <RepoDirs name={name} />
     </div>
   );
 };
