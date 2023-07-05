e4197f031fce6594728e13a2e26d9c3f569e3fb1
Your Name <rghzaiel@ggn-group.com>
2023-07-05 10:21:12 +0100

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import Header from "./components/Header";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Rania Media",
+  description: "Web development tutorials and courses",
+  keywords:
+    "web development, web design, javascript, react, node, angular, vue, html, css",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <Header />
+        <main className="container">{children}</main>
+      </body>
+    </html>
+  );
+}
